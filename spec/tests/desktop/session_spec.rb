05138c7fa9c3c51f2989090e606e2db18a08d4de
bkitchener1 <kitchener.brian@gmail.com>
2014-04-24 15:29:13 -0600

    removing session tests temporarily

--- spec/tests/desktop/session_spec.rb
+++ spec/tests/desktop/session_spec.rb
@@ -20,19 +20,19 @@ feature 'Session' do
   end
 
   scenario 'Cart Session Expiration' do
-    @page = @page.header.SearchFor 'table'
-    @page = @page.GoToFirstProduct(:available).AddToCart
-    @page = ShoppingCartPage.new
-    @page.load
-    $logger.Log("Waiting for 10 minutes to verify shopping cart session has expired")
-    sleep (60 * 10)
-    @page.should have_cart_timed_out_message
-    @page.should have_item_reservation_duration :text=>'00:00'
+    #@page = @page.header.SearchFor 'table'
+    #@page = @page.GoToFirstProduct(:available).AddToCart
+    #@page = ShoppingCartPage.new
+    #@page.load
+    #$logger.Log("Waiting for 10 minutes to verify shopping cart session has expired")
+    #sleep (60 * 10)
+    #@page.should have_cart_timed_out_message
+    #@page.should have_item_reservation_duration :text=>'00:00'
   end
 
   scenario 'Auth Session Expiration' do
-    @page.WaitForSessionToExpire
-    @page.header.should be_all_there
-    @page.should have_text "LOG IN"
+    #@page.WaitForSessionToExpire
+    #@page.header.should be_all_there
+    #@page.should have_text "LOG IN"
   end
 end
